e85f41577638c21a1bcd7c3759057d780c422f89
Bárbara Menezes <bmenezes@ciandt.com>
2020-03-31 16:46:29 -0300

    Ex 9

--- exercises/Exercise10/ex10.js
+++ exercises/Exercise10/ex10.js
@@ -1,12 +1,26 @@
-import requestHttpCharacters from './ex9_1';
+import requestHttpCharacters from '../Exercise9/ex9_1';
 
-async function getAllCharacters() {
+async function getCharactersName() {
     try {
         const response = await requestHttpCharacters();
-        return response.length;
+
+        var names = [];
+        for (var i = 0; i < response.data.length; i++)
+        {
+            names.push(response.data[i].name);
+        }
+
+        // names.map(i => {
+        //     names.push(response.data[i].name);
+        // });
+
+        names.sort();
+
+        return names;
+
       } catch (error) {
         return error;
       }
 }
 
-export default getAllCharacters;
+export default getCharactersName;
--- exercises/Exercise10/ex10.test.js
+++ exercises/Exercise10/ex10.test.js
@@ -1,9 +1,22 @@
-// import requestNamesAPI from './ex10';
+import getCharactersName from './ex10';
+import mockNameCharacters from '../../mocks/APIBrekingBad/mockNameCharacters';
+import mockCharacters from '../../mocks/APIBrekingBad/mockCharacters.js';
+import requestHttpCharacters from '../Exercise9/ex9_1';
+jest.mock('../Exercise9/ex9_1');
 
-// describe('function requestNamesAPI', () => {
-//     it('Should show the lenght of characters list', async () => {
-//          const result = await requestNamesAPI();
+describe('function getNumberOfCharacters', () => {
+    it('Should show the number of characters', async () => {
+        const result = await getCharactersName();
+        console.log(result);
 
-//          expect(result).toEqual("test");
-//     })
-// })
+        expect(result).toEqual(mockNameCharacters);
+    })
+    it('Trows error from an API', async () => {
+        const errorMessage = 'Network Error';
+        requestHttpCharacters.mockRejectedValue(errorMessage);
+
+        await getCharactersName().catch(error => {
+            expect(error).toThrowError(errorMessage);
+          })
+    });
+})
